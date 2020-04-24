4b1fdfd234505db003dc87e54d6c53ade8dfff82
Asad Saeeduddin <masaeedu@gmail.com>
2020-04-23 23:06:28 -0400

    Add `alignAA` for aligning applicative + alternative functors

--- src/Data/Functor/Monoidal/Alignable.hs
+++ src/Data/Functor/Monoidal/Alignable.hs
@@ -1,8 +1,11 @@
 module Data.Functor.Monoidal.Alignable where
 
-import Control.Category.Tensor
+import Prelude hiding (zip, (<*>))
 
+import Control.Category.Tensor
 import Data.Functor.Monoidal.Class
+import Data.Functor.Monoidal.Applicative
+import Data.Functor.Monoidal.Alternative
 import Data.These
 
 align :: Align f => f a -> f b -> f (a ⊠ b)
@@ -10,3 +13,6 @@ align = curry combineF
 
 unalign :: Unalign f => f (a ⊠ b) -> f a × f b
 unalign = uncombineF
+
+alignAA :: (Apply f, Alt f) => f a -> f b -> f (a ⊠ b)
+alignAA fa fb = These <$> fa <*> fb <|> This <$> fa <|> That <$> fb
