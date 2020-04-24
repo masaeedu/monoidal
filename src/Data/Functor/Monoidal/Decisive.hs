04a5671fb7da16e888c683d9f9220f8aa524845c
Asad Saeeduddin <masaeedu@gmail.com>
2020-04-23 23:08:10 -0400

    Add some Decisive instances

--- src/Data/Functor/Monoidal/Decisive.hs
+++ src/Data/Functor/Monoidal/Decisive.hs
@@ -4,6 +4,7 @@ import Control.Category.Tensor
 
 import Data.Functor.Monoidal.Class
 
+import Data.Bifunctor
 import Data.Void
 
 decide :: Decide f => f (a + b) -> f a + f b
@@ -12,3 +13,23 @@ decide = uncombineF
 refute :: Decisive f => f Void -> Void
 refute = discardF @(+) @(+)
 
+instance OpSemigroupal (+) (+) Maybe
+  where
+  uncombineF Nothing = Left Nothing
+  uncombineF (Just (Left a)) = Left $ Just a
+  uncombineF (Just (Right b)) = Right $ Just b
+
+instance OpSemigroupal (+) (+) ((,) x)
+  where
+  uncombineF (x, Left a) = Left $ (x, a)
+  uncombineF (x, Right b) = Right $ (x, b)
+
+instance OpMonoidal (+) (+) ((,) a)
+  where
+  discardF = snd
+
+instance OpSemigroupal (+) (+) []
+  where
+  uncombineF [] = Left []
+  uncombineF (Left a : xs) = first (a:) $ decide xs
+  uncombineF (Right b : xs) = second (b:) $ decide xs
