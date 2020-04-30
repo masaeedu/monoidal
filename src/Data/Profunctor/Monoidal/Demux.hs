4a5ca3d963b3cabd626fbe5964a5cd0169dfe1fc
Asad Saeeduddin <masaeedu@gmail.com>
2020-04-29 20:53:44 -0400

    Add monoidal profunctor instances

--- src/Data/Profunctor/Monoidal/Demux.hs
+++ src/Data/Profunctor/Monoidal/Demux.hs
@@ -1,9 +1,17 @@
 module Data.Profunctor.Monoidal.Demux where
 
 import Control.Category.Tensor
-import Data.Profunctor.Monoidal.Class
+
+import Data.Bifunctor
+import Data.Bifunctor.Joker
+
 import Data.Profunctor
+import Data.Profunctor.Monoidal.Class
+
 import Data.Void
+
+import qualified Data.Functor.Monoidal.Class as F
+import qualified Data.Functor.Monoidal.Decisive as F
 
 demux :: Demux p => p a b -> p c d -> p (a + c) (b + d)
 demux = curry combineP
@@ -17,5 +25,33 @@ fanin = (rmap (either id id) .) . demux
 codemux :: Codemux p => p (a + b) (c + d) -> p a c × p b d
 codemux = uncombineP
 
-partition :: Codemux p => p x (a + b) -> p x a × p x b
-partition = codemux . lmap (either id id)
+partitionCD :: Codemux p => p x (a + b) -> p x a × p x b
+partitionCD = codemux . lmap (either id id)
+
+instance Semigroupal (+) (+) (×) (->)
+  where
+  combineP = uncurry bimap
+
+instance Functor f => Semigroupal (+) (+) (×) (Star f)
+  where
+  combineP (Star x, Star y) = Star $ either (fmap Left . x) (fmap Right . y)
+
+instance Functor f => Monoidal (+) (+) (×) (Star f)
+  where
+  unitP _ = Star $ absurd
+
+instance F.Alt f => Semigroupal (+) (+) (×) (Joker f)
+  where
+  combineP = Joker . F.combineF . bimap runJoker runJoker
+
+instance F.Alternative f => Monoidal (+) (+) (×) (Joker f)
+  where
+  unitP = Joker . F.unitF @(+) @(×)
+
+instance F.Decide f => Semigroupal (+) (+) (×) (Costar f)
+  where
+  combineP (Costar x, Costar y) = Costar $ bimap x y . F.decide
+
+instance F.Decisive f => Monoidal (+) (+) (×) (Costar f)
+  where
+  unitP _ = Costar $ F.refute
--- src/Data/Profunctor/Monoidal/Mux.hs
+++ src/Data/Profunctor/Monoidal/Mux.hs
@@ -1,8 +1,15 @@
 module Data.Profunctor.Monoidal.Mux where
 
-import Control.Category.Tensor
-import Data.Profunctor.Monoidal.Class
+import Control.Category.Tensor hiding (Arrow)
+import Control.Arrow
+
+import Data.Bifunctor
+import Data.Bifunctor.Joker
+
 import Data.Profunctor
+import Data.Profunctor.Monoidal.Class
+
+import qualified Data.Functor.Monoidal.Class as F
 
 mux :: Mux p => p a b -> p c d -> p (a × c) (b × d)
 mux = curry combineP
@@ -10,11 +17,42 @@ mux = curry combineP
 terminal :: Terminal p => p a ()
 terminal = dimap (const ()) id $ unitP @(×) @(×) @(×) ()
 
-zip :: Mux p => p x a -> p x b -> p x (a × b)
-zip = (lmap (\x -> (x, x)) .) . mux
+zipM :: Mux p => p x a -> p x b -> p x (a × b)
+zipM = (lmap (\x -> (x, x)) .) . mux
 
 comux :: Comux p => p (a × b) (c × d) -> p a c × p b d
 comux = uncombineP
 
-undivide :: Comux p => p (a × b) x -> p a x × p b x
-undivide = comux . rmap (\x -> (x, x))
+undivideCM :: Comux p => p (a × b) x -> p a x × p b x
+undivideCM = comux . rmap (\x -> (x, x))
+
+muxA :: Arrow p => p a b × p c d -> p (a × c) (b × d)
+muxA = uncurry (***)
+
+instance Semigroupal (×) (×) (×) (->)
+  where
+  combineP = muxA
+
+instance F.Apply f => Semigroupal (×) (×) (×) (Star f)
+  where
+  combineP (Star x, Star y) = Star $ F.combineF . bimap x y
+
+instance F.Applicative f => Monoidal (×) (×) (×) (Star f)
+  where
+  unitP _ = Star $ F.unitF @(×) @(×)
+
+instance F.Apply f => Semigroupal (×) (×) (×) (Joker f)
+  where
+  combineP (Joker x, Joker y) = Joker $ F.combineF (x, y)
+
+instance F.Applicative f => Monoidal (×) (×) (×) (Joker f)
+  where
+  unitP = Joker . F.unitF @(×) @(×)
+
+instance Functor f => Semigroupal (×) (×) (×) (Costar f)
+  where
+  combineP (Costar x, Costar y) = Costar $ \v -> (x $ fmap fst $ v, y $ fmap snd $ v)
+
+instance Functor f => Monoidal (×) (×) (×) (Costar f)
+  where
+  unitP _ = Costar $ const ()
