7e87431828c5bc6b48ab16d857fe7ca7d78f56e3
Asad Saeeduddin <masaeedu@gmail.com>
2020-07-05 00:24:33 -0400

    Move stuff around, uncomment incoherent fallback instance

--- src/Control/Category/Tensor.hs
+++ src/Control/Category/Tensor.hs
@@ -18,9 +18,92 @@ type (⊠) = These
 infixl 6 +
 infixl 7 ×
 
-class (Category (Arrow t)) => Structure t
+-- {{{ CLASSES
+
+class
+  ( Category (Arrow t)
+  , Bifunctor t
+  ) =>
+  Structure t
   where
   type Arrow t :: * -> * -> *
+
+class Structure t => Associative t
+  where
+  assoc :: Iso (Arrow t) ((x `t` y) `t` z) (x `t` (y `t` z))
+
+class Structure t => Braided t
+  where
+  braid :: Iso (Arrow t) (x `t` y) (y `t` x)
+
+  default braid :: Symmetric t => Iso (Arrow t) (x `t` y) (y `t` x)
+  braid = Iso symm symm
+
+class Braided t => Symmetric t
+  where
+  symm :: Arrow t (x `t` y) (y `t` x)
+
+class Structure t => Unital t
+  where
+  type Unit t :: *
+  lunit :: Iso (Arrow t) (t (Unit t) x) x
+  runit :: Iso (Arrow t) (t x (Unit t)) x
+
+type Tensor t = (Associative t, Unital t)
+
+type Bistructure t p = (Structure t, Structure p, Arrow t ~ Arrow p)
+
+class Bistructure times plus => LaxLDistrib times plus
+  where
+  ldistrib :: Arrow times (x `times` (y `plus` z)) ((x `times` y) `plus` (x `times` z))
+
+class Bistructure times plus => LaxLAnnihil times plus
+  where
+  lnihil :: Arrow times (x `times` Unit plus) (Unit plus)
+
+class Bistructure times plus => LaxRDistrib times plus
+  where
+  rdistrib :: Arrow times ((x `plus` y) `times` z) ((x `times` z) `plus` (y `times` z))
+
+class Bistructure times plus => LaxRAnnihil times plus
+  where
+  rnihil :: Arrow times (Unit plus `times` x) (Unit plus)
+
+class Bistructure times plus => OpLaxLDistrib times plus
+  where
+  opldistrib :: Arrow times ((x `times` y) `plus` (x `times` z)) (x `times` (y `plus` z))
+
+class Bistructure times plus => OpLaxLAnnihil times plus
+  where
+  oplnihil :: Arrow times (Unit plus) (x `times` Unit plus)
+
+class Bistructure times plus => OpLaxRDistrib times plus
+  where
+  oprdistrib :: Arrow times ((x `times` z) `plus` (y `times` z)) ((x `plus` y) `times` z)
+
+class Bistructure times plus => OpLaxRAnnihil times plus
+  where
+  oprnihil :: Arrow times (Unit plus) (Unit plus `times` x)
+
+type LRig times plus =
+  ( LaxLDistrib times plus
+  , LaxLAnnihil times plus
+  , OpLaxLDistrib times plus
+  , OpLaxLAnnihil times plus
+  )
+
+type RRig times plus =
+  ( LaxRDistrib times plus
+  , LaxRAnnihil times plus
+  , OpLaxRDistrib times plus
+  , OpLaxRAnnihil times plus
+  )
+
+type Rig times plus = (LRig times plus, RRig times plus)
+
+-- }}}
+
+-- {{{ STRUCTURE
 
 instance Structure (×)
   where
@@ -34,9 +117,9 @@ instance Structure (⊠)
   where
   type Arrow (⊠) = (->)
 
-class (Structure t, Bifunctor t) => Associative t
-  where
-  assoc :: Iso (Arrow t) ((x `t` y) `t` z) (x `t` (y `t` z))
+-- }}}
+
+-- {{{ ASSOCIATIVE
 
 instance Associative (×)
   where
@@ -56,16 +139,9 @@ instance Associative (⊠)
   where
   assoc = Iso assocThese unassocThese
 
-class Structure t => Braided t
-  where
-  braid :: Iso (Arrow t) (x `t` y) (y `t` x)
+-- }}}
 
-  default braid :: Symmetric t => Iso (Arrow t) (x `t` y) (y `t` x)
-  braid = Iso symm symm
-
-class Braided t => Symmetric t
-  where
-  symm :: Arrow t (x `t` y) (y `t` x)
+-- {{{ BRAIDED, SYMMETRIC
 
 instance Braided (×)
 instance Symmetric (×)
@@ -82,13 +158,9 @@ instance Symmetric (⊠)
   where
   symm = swapThese
 
-class Structure t => Unital t
-  where
-  type Unit t :: *
-  lunit :: Iso (Arrow t) (t (Unit t) x) x
-  runit :: Iso (Arrow t) (t x (Unit t)) x
+-- }}}
 
-type Tensor t = (Associative t, Unital t)
+-- {{{ UNITAL
 
 instance Unital (×)
   where
@@ -108,59 +180,106 @@ instance Unital (⊠)
   lunit = Iso (these absurd id absurd) That
   runit = Iso (these id absurd (const absurd)) This
 
-class (Structure times, Structure plus, Arrow times ~ Arrow plus) => LaxLeftDistributive times plus
-  where
-  ldistrib :: Arrow times (x `times` (y `plus` z)) ((x `times` y) `plus` (x `times` z))
+-- }}}
 
-class (Structure times, Structure plus, Arrow times ~ Arrow plus) => LaxRightDistributive times plus
-  where
-  rdistrib :: Arrow times ((x `plus` y) `times` z) ((x `times` z) `plus` (y `times` z))
+-- {{{ DISTRIBUTIVE
 
-class OpLaxLeftDistributive times plus
-  where
-  opldistrib :: Arrow times ((x `times` y) `plus` (x `times` z)) (x `times` (y `plus` z))
-
-class OpLaxRightDistributive times plus
-  where
-  oprdistrib :: Arrow times ((x `times` z) `plus` (y `times` z)) ((x `plus` y) `times` z)
-
-type LeftRig  times plus = (LaxLeftDistributive  times plus, OpLaxLeftDistributive  times plus)
-type RightRig times plus = (LaxRightDistributive times plus, OpLaxRightDistributive times plus)
-
-type Rig times plus = (LeftRig times plus, RightRig times plus)
-
+-- (×) apparently distributes laxly over any monoidal structure. This fact is probably specific to Hask, since I'm using something like the tensorial strengths of a Bifunctor wrt (×), which are trivial in Hask, but not in an arbitrary category.
 instance
-  ( Structure plus
-  , Bifunctor plus
-  , Arrow plus ~ Arrow (×)
-  ) =>
-  LaxLeftDistributive (×) plus
+  Bistructure (×) plus =>
+  LaxLDistrib (×) plus
   where
   ldistrib (x, p) = bimap (x, ) (x, ) p
 
 instance
-  ( Structure plus
-  , Bifunctor plus
-  , Arrow plus ~ Arrow (×)
-  ) =>
-  LaxRightDistributive (×) plus
+  Bistructure (×) plus =>
+  LaxLAnnihil (×) plus
+  where
+  lnihil = snd
+
+instance
+  Bistructure (×) plus =>
+  LaxRDistrib (×) plus
   where
   rdistrib (p, z) = bimap (, z) (, z) p
 
 instance
-  ( Structure times
-  , Bifunctor times
-  , Arrow times ~ Arrow (+)
-  ) =>
-  OpLaxLeftDistributive times (+)
+  Bistructure (×) plus =>
+  LaxRAnnihil (×) plus
+  where
+  rnihil = fst
+
+-- Any monoidal structure distributes over the cartesian product (from ncatlab). This distributivity probably isn't super useful, but we'll still witness it.
+instance {-# INCOHERENT #-}
+  Bistructure times (×) =>
+  LaxLDistrib times (×)
+  where
+  ldistrib v = (second fst v, second snd v)
+
+instance {-# INCOHERENT #-}
+  Bistructure times (×) =>
+  LaxLAnnihil times (×)
+  where
+  lnihil = const ()
+
+instance {-# INCOHERENT #-}
+  Bistructure times (×) =>
+  LaxRDistrib times (×)
+  where
+  rdistrib v = (first fst v, first snd v)
+
+instance {-# INCOHERENT #-}
+  Bistructure times (×) =>
+  LaxRAnnihil times (×)
+  where
+  rnihil = const ()
+
+-- Dually, any monoidal structure distributes oplaxly over the cocartesian monoidal structure (which is the cartesian monoidal structure in the opposite category).
+instance
+  Bistructure times (+) =>
+  OpLaxLDistrib times (+)
   where
   opldistrib = either (second Left) (second Right)
 
 instance
-  ( Structure times
-  , Bifunctor times
-  , Arrow times ~ Arrow (+)
-  ) =>
-  OpLaxRightDistributive times (+)
+  Bistructure times (+) =>
+  OpLaxLAnnihil times (+)
+  where
+  oplnihil = absurd
+
+instance
+  Bistructure times (+) =>
+  OpLaxRDistrib times (+)
   where
   oprdistrib = either (first Left) (first Right)
+
+instance
+  Bistructure times (+) =>
+  OpLaxRAnnihil times (+)
+  where
+  oprnihil = absurd
+
+{-
+-- The cocartesian monoidal structure distributes laxly over any bifunctor with a tensorial strength wrt Either
+class Wat t
+  where
+  wat :: Either a (t x y) -> t (Either a x) (Either a y)
+
+instance Wat (,)
+  where
+  wat = either (Left &&& Left) (bimap Right Right)
+
+instance Wat Either
+  where
+  wat = either (Left . Left) (bimap Right Right)
+
+instance
+  ( Bistructure (+) plus
+  , Wat plus
+  ) =>
+  LaxLDistrib (+) plus
+  where
+  ldistrib = either (wat . Left) (bimap Right Right)
+-}
+
+-- }}}
