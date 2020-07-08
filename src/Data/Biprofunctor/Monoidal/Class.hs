a3579e798b74543a2bb15169901ff3facca50ffe
Asad Saeeduddin <masaeedu@gmail.com>
2020-07-08 00:11:00 -0400

    Add monoidal biprofunctor classes

---
+++ src/Data/Biprofunctor/Monoidal/Class.hs
@@ -0,0 +1,36 @@
+module Data.Biprofunctor.Monoidal.Class where
+
+import Control.Category.Tensor
+import Data.Biprofunctor
+
+class
+  ( Associative t1
+  , Associative t2
+  , Associative t3
+  , Associative t4
+  , Associative o
+  , Arrow t1 ~ (->)
+  , Arrow t2 ~ (->)
+  , Arrow t3 ~ (->)
+  , Arrow t4 ~ (->)
+  , Arrow o ~ (->)
+  , Biprofunctor bp
+  ) =>
+  Semigroupal t1 t2 t3 t4 o bp
+  where
+  combineBP :: bp a b s t `o` bp a' b' s' t' -> bp (t1 a a') (t2 b b') (t3 s s') (t4 t t')
+
+(-?-) :: Semigroupal t1 t2 t3 t4 (×) bp => bp a b s t -> bp a' b' s' t' -> bp (t1 a a') (t2 b b') (t3 s s') (t4 t t')
+(-?-) = curry combineBP
+
+class
+  ( Tensor t1
+  , Tensor t2
+  , Tensor t3
+  , Tensor t4
+  , Tensor o
+  , Semigroupal t1 t2 t3 t4 o bp
+  ) =>
+  Monoidal t1 t2 t3 t4 o bp
+  where
+  unitBP :: Unit o -> bp (Unit t1) (Unit t2) (Unit t3) (Unit t4)
