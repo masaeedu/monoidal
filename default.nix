3bada276060f4f174f3d1ec4ac2d88e1f9445479
Asad Saeeduddin <masaeedu@gmail.com>
2020-09-09 22:58:50 -0400

    Add tools to shell.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   sources = import ./nix/sources.nix;
   compilerVersion = "ghc883";
   hnix = import sources.iohk-hnix {};
-  pkgs = (import hnix.sources.nixpkgs-2003) hnix.nixpkgsArgs;
+  pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
 in
 pkgs.haskell-nix.cabalProject {
   src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
--- shell.nix
+++ shell.nix
@@ -1,12 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   compilerVersion = "ghc883";
-  ghcide = (import sources.ghcide-nix {})."ghcide-${compilerVersion}";
-  hspkgs = import ./.;
+  hnix = import sources.iohk-hnix {};
+  pkgs = (import hnix.sources.nixpkgs) hnix.nixpkgsArgs;
 in
-hspkgs.shellFor {
+(import ./.).shellFor {
   withHoogle = true;
   buildInputs = [
-    ghcide
+    (pkgs.haskell-nix.tool compilerVersion "cabal-install" { version = "3.2.0.0"; })
+    (pkgs.haskell-nix.tool compilerVersion "hpack"         { version = "0.34.2"; })
+    (pkgs.haskell-nix.tool compilerVersion "ghcid"         { version = "0.8.7";  })
   ];
 }
