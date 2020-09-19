5a58f5be8f80dba1fb9cacb2ce685d48facac6ec
Asad Saeeduddin <masaeedu@gmail.com>
2020-09-19 19:34:29 -0400

    Switch ghcide to haskell-language-server

--- shell.nix
+++ shell.nix
@@ -2,11 +2,13 @@ let
   sources = import ./nix/sources.nix;
   compilerVersion = "ghc883";
   hnix = import sources.iohk-hnix {};
+  hls = import sources.all-hls { version = "0.4.0"; ghc = "8.8.3"; };
   pkgs = (import hnix.sources.nixpkgs-2003) hnix.nixpkgsArgs;
 in
 (import ./.).shellFor {
   withHoogle = true;
   buildInputs = [
+    hls
     (pkgs.haskell-nix.tool compilerVersion "cabal-install" { version = "3.2.0.0"; })
     (pkgs.haskell-nix.tool compilerVersion "hpack"         { version = "0.34.2"; })
     (pkgs.haskell-nix.tool compilerVersion "ghcid"         { version = "0.8.7";  })
